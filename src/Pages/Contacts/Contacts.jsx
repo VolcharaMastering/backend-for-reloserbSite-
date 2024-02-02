bee9da815a05a74cd827001865707b446c6c4925
Aleksey <vmstr@vmstr.ru>
2024-02-02 16:21:12 +0100

    contact page/no form

--- src/Pages/Contacts/Contacts.jsx
+++ src/Pages/Contacts/Contacts.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import "./Contacts.scss";
+import ContactButtonsBlock from "../../components/ContactButtonsBlock/ContactButtonsBlock";
+import { useResize } from "../../utils/hooks/useResize";
+import BlockWithContacts from "../../components/BlockWithContacts/BlockWithContacts";
 
 function Contacts() {
-  return <div>Contacts</div>;
+  // const [ setContact] = useState("main");
+  const screenSize = useResize();
+
+  // const handleSetContact = (gotContact) => {
+  //   setContact(gotContact);
+  // };
+
+  return (
+    <section className="contacts">
+      <ContactButtonsBlock size={screenSize.trakResolutionValue} />
+      <BlockWithContacts size={screenSize.trakResolutionValue} />
+    </section>
+  );
 }
 
 export default Contacts;
