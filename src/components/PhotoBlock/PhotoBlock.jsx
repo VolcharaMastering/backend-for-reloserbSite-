9f049af5b580a8c7e9ae20b09542ce73085764a5
Aleksey <vmstr@vmstr.ru>
2024-02-13 12:44:03 +0100

    consult page

--- src/components/PhotoBlock/PhotoBlock.jsx
+++ src/components/PhotoBlock/PhotoBlock.jsx
@@ -1,19 +1,19 @@
 import "./PhotoBlock.scss";
 import generateRandomKey from "../../utils/keyGenerator";
 import AboutImg from "../UI/AboutImg/AboutImg";
-import someImg from "../../assets/coworkPictures/DSC00085.jpg";
-function PhotoBlock({ title, images, size }) {
+
+function PhotoBlock({ images, title, description, size, blockTitle }) {
   const getRandomKey = () => {
     const index = generateRandomKey();
     return index;
   };
   return (
     <figure className="photo-block">
-      <h2 className="photo-block__title">{title}</h2>
+      <h2 className="photo-block__title">{blockTitle}</h2>
       <nav className={`photo-block__navigation ${size}`}>
-        {/* {images.map((photo) => ( */}
-        <AboutImg key={getRandomKey} linkToImg={someImg} imgCaption="someimg" imgInfo="" />
-        {/* ))} */}
+        {images.map((photo) => (
+          <AboutImg key={getRandomKey} linkToImg={photo} imgCaption={title} imgInfo={description} />
+        ))}
       </nav>
     </figure>
   );
