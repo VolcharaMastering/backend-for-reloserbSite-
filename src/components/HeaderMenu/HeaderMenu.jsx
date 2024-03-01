01c833e3014aa976f281f38b8fb43552764b2811
Aleksey <vmstr@vmstr.ru>
2024-03-01 12:15:36 +0100

    bugfix and headericons

--- src/components/HeaderMenu/HeaderMenu.jsx
+++ src/components/HeaderMenu/HeaderMenu.jsx
@@ -14,38 +14,43 @@ function HeaderMenu({ size, mobile }) {
   return (
     <nav className="header__menu">
       <ul className={`header__menu-block ${size}`}>
-        {/* <li className={`header__menu-link ${size}`}>
-          <TextLink location={location} linkTo="/" size={size} />
-        </li>
-        <li className={`header__menu-link ${size}`}>
-          <TextLink location={location} linkTo="/consult" size={size} />
-        </li>
-        <li className={`header__menu-link ${size}`}>
-          <TextLink location={location} linkTo="/coworking" size={size} />
-        </li>
-        <li className={`header__menu-link ${size}`}>
-          <TextLink location={location} linkTo="/real-estate" size={size} />
-        </li>
-        <li className={`header__menu-link ${size}`}>
-          <TextLink location={location} linkTo="/cargo" size={size} />
-        </li>
-        <li className={`header__menu-link ${size}`}>
-          <TextLink location={location} linkTo="/visarun" size={size} />
-        </li>
-        <li className={`header__menu-link ${size}`}>
-          <TextLink location={location} linkTo="/lang" size={size} />
-        </li>
-        <li className={`header__menu-link ${size}`}>
-          <TextLink location={location} linkTo="/voffice" size={size} />
-        </li>
-        <li className={`header__menu-link ${size}`}>
-          <TextLink location={location} linkTo="/faq" size={size} />
-        </li> */}
-        {iconsArray.map((icon) => (
-          <li key={getRandomKey} className={`header__menu-link ${size}`}>
-            <HeaderDynamicIcon icons={icon} location={location} size={size} />
-          </li>
-        ))}
+        {mobile ? (
+          <>
+            <li className={`header__menu-link ${size}`}>
+              <TextLink location={location} linkTo="/" size={size} />
+            </li>
+            <li className={`header__menu-link ${size}`}>
+              <TextLink location={location} linkTo="/consult" size={size} />
+            </li>
+            <li className={`header__menu-link ${size}`}>
+              <TextLink location={location} linkTo="/coworking" size={size} />
+            </li>
+            <li className={`header__menu-link ${size}`}>
+              <TextLink location={location} linkTo="/real-estate" size={size} />
+            </li>
+            <li className={`header__menu-link ${size}`}>
+              <TextLink location={location} linkTo="/cargo" size={size} />
+            </li>
+            <li className={`header__menu-link ${size}`}>
+              <TextLink location={location} linkTo="/visarun" size={size} />
+            </li>
+            <li className={`header__menu-link ${size}`}>
+              <TextLink location={location} linkTo="/lang" size={size} />
+            </li>
+            <li className={`header__menu-link ${size}`}>
+              <TextLink location={location} linkTo="/voffice" size={size} />
+            </li>
+            <li className={`header__menu-link ${size}`}>
+              <TextLink location={location} linkTo="/faq" size={size} />
+            </li>
+          </>
+        ) : (
+          iconsArray.map((icon) => (
+            <li key={getRandomKey()} className={`header__menu-link ${size}`}>
+              <HeaderDynamicIcon icons={icon} location={location} size={size} />
+            </li>
+          ))
+        )}
         {mobile && (
           <li className={`header__menu-link ${size}`}>
             <TextLink location={location} linkTo="/contacts" size={size} />
