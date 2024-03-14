74839e714c1b029428095fea8247f1d1e820723a
Aleksey <vmstr@vmstr.ru>
2024-03-14 14:49:39 +0100

    refactor of links in text

--- src/components/UI/DropDownListItem/DropDownListItem.jsx
+++ src/components/UI/DropDownListItem/DropDownListItem.jsx
@@ -1,16 +1,39 @@
 /* eslint-disable no-nested-ternary */
 import { NavLink } from "react-router-dom";
 import "./DropDownListItem.scss";
+import generateRandomKey from "../../../utils/keyGenerator";
 
 function DropDownListItem({ listItem, size }) {
+  const getRandomKey = () => {
+    const index = generateRandomKey();
+    return index;
+  };
   return (
     <li className={`drop-list-item ${size} `}>
       {listItem.text ? (
-        <p className={`drop-list-item__text ${size}`}>{listItem.text}</p>
+        <p className={`drop-list-item__text ${size}`}>
+          {listItem.text.split(/({.*?})/).map((item) => {
+            if (item.includes("{")) {
+              const linkData = JSON.parse(item);
+              return (
+                <NavLink
+                  to={linkData.url}
+                  key={getRandomKey()}
+                  target="_blank"
+                  className={`drop-list-item__text_linked ${size}`}
+                >
+                  {linkData.replaceText}
+                </NavLink>
+              );
+            }
+            return item;
+          })}
+        </p>
       ) : listItem.subText ? (
         <p className={`drop-list-item__sub-text ${size}`}>{listItem.subText}</p>
       ) : (
         <NavLink
+          target="_blank"
           to={listItem.href.src}
           className={`drop-list-item__link drop-list-item__text ${size}`}
         >
