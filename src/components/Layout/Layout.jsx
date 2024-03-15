df8a817c8e4a5cfe9930023b65a61434eee44012
Aleksey <vmstr@vmstr.ru>
2024-03-15 11:11:48 +0100

    preview links. in test

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,13 +7,13 @@ function Layout({ children, title, description, seoName, seoContent, page, ogIma
       <Helmet>
         <title>{title}</title>
         <meta name="description" content={description} />
-        <meta property="og:image" content={ogImage} />
+        {/* <meta property="og:image" content={ogImage} />
         <meta property="og:title" content={`${ogData.ogTitle[page]}`} />
         <meta property="og:description" content={`${ogData.ogDescription[page]}`} />
         <meta property="og:type" content="website" />
         <meta property="og:url" content={`${ogData.ogUrl[page]}`} />
         <meta property="og:site_name" content="ReloSerb. Переезд и легализация в Сербии" />
-        <meta property="og:locale" content="ru_RU" />
+        <meta property="og:locale" content="ru_RU" /> */}
         {seoName && <meta name={seoName} content={seoContent} />}
       </Helmet>
       {children}
