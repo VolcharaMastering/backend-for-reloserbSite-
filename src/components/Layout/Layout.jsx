e20b845a8df1c51ac02c83f2365e8a95be42da59
Aleksey <vmstr@vmstr.ru>
2024-03-15 13:07:37 +0100

    preview links. in test

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,19 +1,11 @@
 import { Helmet } from "react-helmet";
-import { ogData } from "../../content/metaInfo.json";
 
-function Layout({ children, title, description, seoName, seoContent, page, ogImage }) {
+function Layout({ children, title, description, seoName, seoContent }) {
   return (
     <>
       <Helmet>
         <title>{title}</title>
         <meta name="description" content={description} />
-        {/* <meta property="og:image" content={ogImage} />
-        <meta property="og:title" content={ogData.ogTitle[page] || title} />
-        <meta property="og:description" content={ogData.ogDescription[page] || description} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={ogData.ogUrl[page]} />
-        <meta property="og:site_name" content="ReloSerb. Переезд и легализация в Сербии" />
-        <meta property="og:locale" content="ru_RU" /> */}
         {seoName && <meta name={seoName} content={seoContent} />}
       </Helmet>
       {children}
